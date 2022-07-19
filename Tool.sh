893172847cd58b3a9a88541c8581c279dd9cdc78
Arturo254 <87346871+Arturo254@users.noreply.github.com>
2022-07-18 20:59:29 -0500

    Add files via upload

---
+++ Tool.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+#Black = 0;30
+#Dark grey = 1;30
+#Blue = 0;34
+#Light blue = 1;34
+#Green = 0;32
+#Light green = 1;32
+#cyan = 0;36
+#Clear Cyan = 1;36
+#Red = 0.31
+#Light red = 1;31
+#Purple = 0.35
+#Light purple = 1;35
+#Coffee = 0;33
+#Yellow = 1;33
+#Grey = 0.37
+#White = 1;37
+
+clear
+
+
+echo -e "\e[34m████████╗ ██████╗  ██████╗ ██╗     ███████╗"
+echo -e "\e[34m╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝"
+echo -e "\e[34m   ██║   ██║   ██║██║   ██║██║     ███████╗"
+echo -e "\e[34m   ██║   ██║   ██║██║   ██║██║     ╚════██║"
+echo -e "\e[34m   ██║   ╚██████╔╝╚██████╔╝███████╗███████║ "
+echo -e "\e[34m   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝"
+
+echo -e "\e[1;37m  GITHUB: https://github.com/Arturo254    "
+echo "							   "
+
+
+echo -e "\e[31m   INSTALANDO LAS DEPENDENCAS NECESARIAS   "
+
+
+echo "							"
+echo "							"
+echo "							"
+
+
+sleep 5
+
+echo -e "\e[1;37m       "
+pkg install python2 -y
+pkg install python3 -y
+pkg install  wget -y
+pkg install tmate -y
+pkg install tmux -y
+pkg install openssl -y
+pkg install micro -y
+pkg install nano -y
+pkg install emacs -y
+
+clear 
+echo -e "\e[32m       INSTALANDO  RSYNC  (RESPALDO)        "
+sleep 3
+ 
+echo -e "\e[1;37m       "
+
+pkg install rsync -y
+
+clear 
+
+echo -e "\e[1;30m	  INSTALANDO FIGLET 		"
+
+pkg install figlet -y
+
+clear 
+
+echo -e "\e[1;35m  INSTALANDO LO FALTANTE EN TU SISTEMA/TERMINAL, ESPERA TARDARA UN POCO DEPENDE TU INTERNET"
+sleep 2 
+
+
+echo -e "\e[1;37m       "
+
+pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install hydra -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrad
+
+clear
+ 
+echo -e "\e[34m INSTALANDO TODOS LOS PAQUETES (PKG) APROX. 21 "
+
+echo -e "\e[1;37m       "
+sleep 1
+ 
+pkg install tmate -y
+pkg install libfm -y
+pkg install  tmate -y 
+pkg install wv-tools -y 
+pkg install openjdk-17
+pkg install tdb-tools -y 
+pkg install termux-api -y 
+pkg install termux-style -y 
+pkg install bat -y 
+pkg update && pkg upgrade -y 
+pkg install PyQt4 pkg install python2 -y 
+pkg install PyQt4 -y 
+pkg install nmap -y 
+pkg install googlesearch -y
+pkg install termux-x11 -y 
+pkg install mtools -y 
+pkg upgrade -y 
+pkg update -y 
+pkg install exiftool -y                                                                                               pkg install xcowsay                                                                                                   pkg install cowsay
+pkg install nodejs -y
+pkg install unzip -y 
+pkg install wget -y
+pkg install php python -y 
+pkg install Packaging -y 
+pkg install python php -y 
+pkg install  pip3 -y 
+pkg install Requests -y 
+pkg install openssh -y
+pkg install unstable-repo -y
+pkg install localxpose -y 
+pkg install micro -y 
+pkg install termux-editor -y 
+pkg install proot -y 
+pkg install gettext -y 
+pkg install jp2a -y 
+pkg install lnd -y       
+pkg install nodejs-lts -y 
+pkg install php  -y                                                                                          
+pkg install NeoFetch -y
+pkg install -y git python curl wget php openssh
+pkg install proot -y
+pkg install proot -y 
+pkg install emacs -y 
+pkg install html-xml-utils -y 
+pkg install tsu -y 
+
+clear
+
+sleep 2
+
+clear
+
+echo -e "\e[1;34m 		INSTALANDO MODULOS APT (APTITUDE) 		"
+
+echo -e "\e[1;37m       "
+sleep 2 
+
+apt upgrade -y
+apt update -y 
+apt-get install httrack -y 
+apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y 
+apt-get upgrade -y 
+apt-get update -y 
+apt-get install python2.7 -y                                                                             
+apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y
+apt install php -y
+apt  install PyQt4 -y 
+apt update -y
+apt update && apt upgrade -y
+apt install python3 -y
+apt install python3 python3-pip php -y
+apt-get install python3-pip -y
+apt-get install python3 -y
+apt-get install python3-dev -y
+apt upgrade -y
+apt install requests -y
+apt install restic -y
+apt-get install xterm -y
+apt install apache2 -y
+apt-get install lolcat -y
+sudo apt-get install lolcat -y
+apt install termimage -y
+apt install localxpose -y
+apt install tigervnc -y
+apt upgrade && apt update -y
+apt install termux-editor -y
+apt install figlet -y
+apt install openssh -y
+apt-get install -y
+apt install calcurse-caldav -y
+apt-get remove --purge  calcurse-caldav -y
+apt install tmate -y
+apt install proot -y
+apt install byobu -y
+sudo apt-get install jp2a -y
+apt install php -y
+apt install neofetch -y
+apt install nmap -y
+apt install unzip -y
+apt upgrade -y
+apt install cmd -y
+apt update && apt upgrade -y
+apt install python2 -y
+apt update -y && apt upgrade -y
+apt install ngrok -y 
+apt install vim -y
+apt-get install screenfetch -y 
+apt install xcowsay -y 
+
+
+echo -e "\e[1;32m POR AHORA ESTE ES EL FIN ESPERA A LA SIGUINETE ACTUALIZACION 😄  BY ARTURO       "
